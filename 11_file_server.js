44430431fa85c80072b29bbc7ac61e05760ad2bb
Stephen Mayeux <stephenmayeux@gmail.com>
2017-03-11 11:06:56 -0600

    challenges 10 - 14

---
+++ 11_file_server.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const http = require('http');
+
+const port = process.argv[2];
+const file = process.argv[3];
+
+const server = http.createServer((req, res) => {
+  const stream = fs.createReadStream(file, { encoding: 'utf8' });
+  stream.pipe(res);
+});
+
+server.listen(port);
